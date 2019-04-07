ef099ded90c95e89cc16e299caa7bc46c332d2d2
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2019-04-07 15:10:01 +0100

    add views

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -1,24 +1,25 @@
 class TeamMembersController < ApplicationController
     def index
         @team = Team.find(params[:team_id])
-        @team_member = TeamMember.all
+        @users = User.all
     end
 
     def new
-        @team = Team.find(params[:team_id])
         @team_member = TeamMember.new
     end
 
     def create 
+        @team = Team.find(params[:team_id])
+        @user = User.find(params[:user_id])
+        @team_member = TeamMember.new  
+        @team_member.user = @user
+        @team_member.team = @team
+        @team_member.save
+
+        redirect_to teams_path
     end 
 
     def update 
     end
-
-    def destroy
-        @team_member = TeamMember.find(params[:id])
-        @team_member.destroy
-    end
-
 
 end
